712116f19fec186682d69f79f7cdc644a7979c2b
Marin Bibot <45786643+Marin-b@users.noreply.github.com>
2021-08-31 10:14:54 +0200

    Lecture code

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,39 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    # user # current user
+    # record # instance of object that we are displaying (@restaurant)
+    owner? || admin?
+  end
+
+  def update?
+    owner? || admin?
+  end
+
+  def destroy?
+    owner? || admin?
+  end
+
+  private
+
+  def owner?
+    record.user == user
+  end
+
+  def admin?
+    user.admin
+  end
+end
